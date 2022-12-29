0cf25830a2d25387971f000899c3bcf724e4e1b6
ognjen-pavlovic <ognjen.pavlovic@prvabeogim.edu.rs>
2022-12-29 13:55:13 +0100

    Update 4.

---
+++ ESDnevnik2/Login.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ESDnevnik2
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+                MessageBox.Show("Unesite email i/ili lozinku!");
+            else
+            {
+                try
+                {
+                    SqlConnection veza = Konekcija.Connect();
+                    string loginemail = "SELECT * FROM osoba WHERE email = " + textBox1.Text;
+                    SqlCommand komanda = new SqlCommand(loginemail, veza);
+                    SqlDataAdapter adapter = new SqlDataAdapter(komanda);
+                    DataTable tabela = new DataTable();
+                    adapter.Fill(tabela);
+                    int brojac = tabela.Rows.Count;
+                    if (brojac == 1)
+                    {
+                        if (String.Compare(tabela.Rows[0]["pass"].ToString(), textBox2.Text) == 0)
+                        {
+                            MessageBox.Show("Login Successful!");
+                        }
+                        else
+                            MessageBox.Show("Netacan email i/ili sifra!");
+                    }
+                    else
+                        MessageBox.Show("Netacan email i/ili sifra!");
+                }
+                catch (Exception Greska)
+                {
+                    MessageBox.Show(Greska.Message);
+                }
+            }
+        }
+    }
+}
